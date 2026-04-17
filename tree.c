852a9d5af81b4084339b7ebaa2d11bb91b0bb146
Shriranninin <shriranjinin2gmail.com>
2026-04-17 14:40:56 +0000

    phase 3 : chnages made

--- tree.c
+++ tree.c
@@ -112,10 +112,9 @@ int tree_serialize(const Tree *tree, void **data_out, size_t *len_out) {
 }
 
 // ─── TODO: Implement these ──────────────────────────────────────────────────
-
 // Build a tree hierarchy from the current index and write all tree
 // objects to the object store.
-//
+
 // HINTS - Useful functions and concepts for this phase:
 //   - index_load      : load the staged files into memory
 //   - strchr          : find the first '/' in a path to separate directories from files
